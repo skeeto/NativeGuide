eb668693128b33a9fd9c54dfd1fc89252201898b
Christopher Wellons <mosquitopsu@gmail.com>
2011-11-01 14:17:56 -0400

    Oops, wrong DLL name.

--- test/com/nullprogram/guide/NativeGuideTest.java
+++ test/com/nullprogram/guide/NativeGuideTest.java
@@ -10,7 +10,7 @@ public class NativeGuideTest extends TestCase {
         try {
             prepare(LINUX_32,   root + "linux32/libguide.so");
             prepare(LINUX_64,   root + "linux64/libguide.so");
-            prepare(WINDOWS_32, root + "windows32/libguide.dll");
+            prepare(WINDOWS_32, root + "windows32/guide.dll");
             System.loadLibrary("guide");
         } catch (java.io.IOException e) {
             fail("Could not prepare library: " + e);
