8e100cb0a4b17020fdd8df60d6be19cd613eefa2
Christopher Wellons <mosquitopsu@gmail.com>
2011-11-02 10:32:10 -0400

    Tidy up the unit test a little.

--- test/com/nullprogram/guide/NativeGuideTest.java
+++ test/com/nullprogram/guide/NativeGuideTest.java
@@ -7,12 +7,11 @@ import junit.framework.TestCase;
 public class NativeGuideTest extends TestCase {
 
     public final void testIsArchitecture() {
-        String root = "/com/nullprogram/guide/";
         try {
-            NativeGuide.prepare(Arch.LINUX_32,   root + "linux32/libguide.so");
-            NativeGuide.prepare(Arch.LINUX_64,   root + "linux64/libguide.so");
-            NativeGuide.prepare(Arch.WINDOWS_32, root + "windows32/guide.dll");
-            NativeGuide.prepare(Arch.WINDOWS_64, root + "windows64/guide.dll");
+            NativeGuide.prepare(Arch.LINUX_32,   "linux32/libguide.so");
+            NativeGuide.prepare(Arch.LINUX_64,   "linux64/libguide.so");
+            NativeGuide.prepare(Arch.WINDOWS_32, "windows32/guide.dll");
+            NativeGuide.prepare(Arch.WINDOWS_64, "windows64/guide.dll");
             System.loadLibrary("guide");
         } catch (java.io.IOException e) {
             fail("Could not prepare library: " + e);
