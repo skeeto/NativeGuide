b179ff8696bd61c639d68cc968c9984e05ec2a7f
Christopher Wellons <mosquitopsu@gmail.com>
2011-11-03 10:48:13 -0400

    Elaborate in package-info.

--- src/com/nullprogram/guide/package-info.java
+++ src/com/nullprogram/guide/package-info.java
@@ -1,4 +1,8 @@
 /**
- * Utilities for loading distributed native libraries.
+ * Utilities for loading distributed native libraries from
+ * resources. This allows you to pack your native libraries into your
+ * distributed JAR file, making their use completely transparent to
+ * the end-user. It also manages the java.library.path property, so no
+ * need to hack OS detection in some JAR wrapper ahead of time.
  */
 package com.nullprogram.guide;
